3b923a4fc7f7552555a2d87fc36b1d7b7a02939c
Plambi <adouc78@gmail.com>
2025-11-08 16:37:54 -0700

    PageContainer Title
    - Added children node props which align with the title of PageContainer
    - Fixed the awful color being used with the plain variant

--- src/containers/PageContainer.tsx
+++ src/containers/PageContainer.tsx
@@ -5,6 +5,8 @@ type PageContainerProps = {
     description?: string;
     children: ReactNode;
     className?: string;
+    leftTitleNode?: ReactNode;
+    rightTitleNode?: ReactNode;
 };
 
 export function PageContainer({
@@ -12,13 +14,27 @@ export function PageContainer({
     title,
     description,
     className,
+    leftTitleNode,
+    rightTitleNode,
 }: PageContainerProps) {
     return (
         <div
             className={`max-w-5xl mx-auto flex flex-col space-y-6 xs:mx-0 p-6 sm:p-4 ${className}`}
         >
             <div className="mb-6">
-                <h1 className="text-2xl text-center font-bold">{title}</h1>
+                <div className="flex items-center justify-between w-full relative">
+                    {/* Left node */}
+                    <div className="flex-shrink-0">{leftTitleNode}</div>
+
+                    {/* Centered title */}
+                    <h1 className="absolute left-1/2 transform -translate-x-1/2 text-2xl font-bold">
+                        {title}
+                    </h1>
+
+                    {/* Right Area */}
+                    <div className="flex-shrink-0">{rightTitleNode}</div>
+                </div>
+
                 <h2 className="text-lg text-center font-semibold">
                     {description}
                 </h2>
